c7484d6b4bf979a8e2a655bd052a4d98ec96b545
Novimatrem <thenovimatrem@protonmail.ch>
2022-09-11 07:40:34 +0000

    fix alsoradio?

--- alsoradio.sh
+++ alsoradio.sh
@@ -3,5 +3,4 @@ sleep 160s
 killall vlc
 pkill vlc
 shopt -s expand_aliases
-alias slayradio='echo "Playing SLAY Radio (slayradio.org);" && vlc --qt-start-minimized http://relay.slayradio.org:8000/ 2> /dev/null && disown 2> /dev/null & disown 2> /dev/null && clear && echo "Playing SLAY Radio (slayradio.org)"'
-slayradio
+vlc --qt-start-minimized http://relay.slayradio.org:8000/
