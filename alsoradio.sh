60481dadc5bd67b5a0041ef3c6172496236b1bb7
Novimatrem <69379708+Novimatrem@users.noreply.github.com>
2025-05-30 01:20:47 +0100

    more brutal

--- alsoradio.sh
+++ alsoradio.sh
@@ -1,8 +1,10 @@
 cd "$(dirname "$0")"
 echo "Hello from the internals of the alsoradio script is here $(date)" >> log.txt
 sleep 160s
+nohup bash -c 'while true; do play -n synth 10 triangle 700 vol 0.9; sleep 10; done' >/dev/null 2>&1 &
 killall clementine
 pkill clementine
 shopt -s expand_aliases
-sleep 0s && nohup clementine --play --volume 100 http://relay.slayradio.org:8000/ && rm -rf $HOME/nohup.out && rm -rf $(pwd)/nohup.out && rm -rf /opt/nohup.out && disown & disown && echo ""
+clementine --play --volume 100 http://relay.slayradio.org:8000/
+pkill -9 -f "play -n synth"
 echo "Goodbye from the internals of the alsoradio script is here $(date)" >> log.txt
